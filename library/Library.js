91b56b0fde90a2f6e9f67d1de1162ae3c13a6f95
AlexMarch95 <bigbenfaq@mail.ru>
2023-12-13 20:25:52 +0300

    feat: relize new finish slider carusel

--- library/Library.js
+++ library/Library.js
@@ -1,17 +1,26 @@
-let offset = 0;
+// working slider start
+let offset = -470;
 const sliderLine = document.querySelector('.slider-line');
 
-document.querySelector('.dots.one').addEventListener('click', function () {
-    offset = offset + 450 ; 
-    if (offset > 1200) {
-        offset = 0
+document.querySelector('.dots.one').addEventListener('click' , function () {
+    offset = offset + 470;
+    if( offset > 0) {
+        offset = 0 
     }
-    sliderLine.style.left = -offset + 'px';
-})     
+    sliderLine.style.left = -offset +'px';
+})                
+document.querySelector('.dots.two').addEventListener('click', function () {
+    offset = 470;
+    if(offset > 470 ) {
+        offset = 470
+    }
+    sliderLine.style.left = -offset + 'px'
+} )             
 document.querySelector('.dots.three').addEventListener('click', function () {
-    offset = offset - 450 ; 
-    if (offset < 0) {
-        offset = 1350
+    offset = 940
+    if (offset > 940) {
+        offset = 940
     }
     sliderLine.style.left = -offset + 'px';
-})                                                                                                                                                                                               
+})      
+// working slider end 
