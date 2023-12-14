5f294ebe7e59b7835f53336df40e5a07d2066013
AlexMarch95 <bigbenfaq@mail.ru>
2023-12-14 13:32:33 +0300

    fix: fix some position in Slider and add two picture

--- library/Library.js
+++ library/Library.js
@@ -3,24 +3,18 @@ let offset = -470;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.dots.one').addEventListener('click' , function () {
-    offset = offset + 470;
-    if( offset > 0) {
-        offset = 0 
-    }
+    offset = 470;
+    
     sliderLine.style.left = -offset +'px';
 })                
 document.querySelector('.dots.two').addEventListener('click', function () {
-    offset = 470;
-    if(offset > 470 ) {
-        offset = 470
-    }
+    offset = 950;
+    
     sliderLine.style.left = -offset + 'px'
 } )             
 document.querySelector('.dots.three').addEventListener('click', function () {
-    offset = 940
-    if (offset > 940) {
-        offset = 940
-    }
+    offset = 1430
+   
     sliderLine.style.left = -offset + 'px';
 })      
 // working slider end 
